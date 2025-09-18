564f465c68560e6ae313f053ee0b680d30198928
Nguyen Duc Cuong <cuongndhe186815@fpt.edu.vn>
2025-09-18 11:22:52 +0700

    refreshtoken

--- user-service/src/main/java/com/example/user_service/controller/AuthController.java
+++ user-service/src/main/java/com/example/user_service/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.user_service.service.JwtService;
 import com.example.user_service.repository.UserRepository;
 import com.example.user_service.model.User;
+import io.jsonwebtoken.Claims;
 
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.cache.annotation.CacheEvict;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import lombok.RequiredArgsConstructor;
@@ -34,11 +36,13 @@ public class AuthController {
         if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
         }
-        String token = jwtService.generateToken(user);
-        return ResponseEntity.ok(Map.of("token", token));
+        String tokenAccess = jwtService.generateTokenAccess(user);
+        String tokenRefresh = jwtService.generateRefreshToken(user);
+        return ResponseEntity.ok(Map.of("tokenAccess", tokenAccess,"tokenRefresh", tokenRefresh));
     }
 
     @PostMapping("/register")
+    @CacheEvict(value = "allUsers", allEntries = true)
     public ResponseEntity<?> register(@RequestBody Map<String, String> body) {
         String email = body.get("email");
         String password = body.get("password");
@@ -54,4 +58,27 @@ public class AuthController {
         userRepository.save(newUser);
         return ResponseEntity.ok(Map.of("message", "User registered successfully"));
     }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<?> takeAccessToken(@RequestBody Map<String, String> body) {
+        String refreshToken = body.get("refreshToken");       
+        Claims claims = jwtService.extractClaims(refreshToken);
+        
+        // Kiểm tra token type phải là "refresh"
+        String tokenType = claims.get("type", String.class);
+        if (!"refresh".equals(tokenType)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token type");
+        }
+        
+        Long userId = claims.get("userId", Long.class);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token"));
+        if (!jwtService.isTokenValid(refreshToken)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token");
+        }
+        String accessToken = jwtService.generateTokenAccess(user);
+        String newRefreshToken = jwtService.generateRefreshToken(user);
+        return ResponseEntity.ok(Map.of("accessToken", accessToken, "refreshToken", newRefreshToken));
+    }
+    
 }
--- user-service/src/main/java/com/example/user_service/service/JwtService.java
+++ user-service/src/main/java/com/example/user_service/service/JwtService.java
@@ -1,6 +1,6 @@
 package com.example.user_service.service;
 
-import com.example.user_service.model.User;
+import com.example.user_service.model.*;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -13,17 +13,29 @@ import javax.crypto.SecretKey;
 public class JwtService {
     private static final String SECRET_KEY = "my-very-long-and-secure-jwt-secret-key-123456";
 
-    public String generateToken(User user) {
+    public String generateTokenAccess(User user) {
         SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
         return Jwts.builder()
             .subject(user.getEmail())
             .claim("userId", user.getId())
+            .claim("type", "access")
             .issuedAt(new Date(System.currentTimeMillis()))
-            .expiration(new Date(System.currentTimeMillis() + 86400000))
+            .expiration(new Date(System.currentTimeMillis() + 900000)) // 15 phút
             .signWith(key)
             .compact();
     }
-    
+    public String generateRefreshToken(User user){
+        SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+        return Jwts.builder()
+            .subject(user.getEmail())
+            .claim("userId",user.getId())
+            .claim("type", "refresh")
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + 604800000)) // 7 ngày
+            .signWith(key)
+            .compact();
+    }
+
     public Claims extractClaims(String token) {
         SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
         return Jwts.parser()
